Fix bugs of gen_results for mAP calculation

dev/compressor.py
@@ -17,34 +17,71 @@ from dev import graph_editor as ge
 from detect import *
 from utils.imgutils import *
 
+
 class Compressor:
     def __init__(self):
         self.buffer = []
 
-
     def fill_buffer(self, data):
+        self.buffer = []
         compressed_data = self.compress(data)
         self.buffer.append(compressed_data)
         return 0
 
     def read_buffer(self):
-        data = self._get_from_buf()
-        data.seek(0)
-        decompressed_array = np.load(data)['arr_0']
-        return decompressed_array
-    
+        data = self.__get_from_buf()
+        return self.extract(data)
+
     def compress(self, data):
+        res = self.__zip_enc(data)
+        # res = self.__float_split_mantissa_exponent_enc(data)
+        return res
+
+    def extract(self, data):
+        res = self.__zip_dec(data)
+        # res = self.__float_split_mantissa_exponent_dec(data[0], data[1])
+        return res
+
+    def __get_from_buf(self):
+        return self.buffer[0]
+
+    def __zip_enc(self, data):
         data_copy = copy.copy(data)
         # print(sys.getsizeof(data_copy))
         # data_copy = data_copy.astype(dtype=np.int8)
         # print(sys.getsizeof(data_copy))
         compressed_array = io.BytesIO()
         np.savez_compressed(compressed_array, data_copy)
-        print(sys.getsizeof(compressed_array))
+        # print(sys.getsizeof(compressed_array))
         return compressed_array
-    
-    def _get_from_buf(self):
-        return self.buffer[0]
+
+    def __zip_dec(self, data):
+        data.seek(0)
+        decompressed_array = np.load(data)['arr_0']
+        return decompressed_array
+
+    def __float_split_mantissa_exponent_enc(self, x):
+        """
+        x: numpy array, dtype=float
+        return: mantissa and exponent of elements
+        """
+        data = copy.copy(x)
+        print(sys.getsizeof(data))
+        mant, exp = np.frexp(data)
+        exp = exp.astype(dtype=np.int8)
+        a = 1000
+        tmp = mant * a
+        mant = tmp.astype(dtype=np.int16)
+        print(sys.getsizeof(mant), sys.getsizeof(exp))
+        mant_zip = self.__zip_enc(mant)
+        exp_zip = self.__zip_enc(exp)
+        print(sys.getsizeof(mant_zip), sys.getsizeof(exp_zip))
+        return mant, exp
+
+    def __float_split_mantissa_exponent_dec(self, mant, exp):
+        a = 1000
+        mant = mant / a
+        return np.ldexp(mant, exp)
 
 
 if __name__ == "__main__":
@@ -64,30 +101,34 @@ if __name__ == "__main__":
     compressor = Compressor()
     # main loop
     img_orig = cv2.imread('./pedes_images/01-20170320211735-19.jpg')
+    img_orig = cv2.imread('./cocoapi/images/val2017/000000000724.jpg')
     img = preprocess_image(img_orig)
     start = time.time()
     output_feature = sess1.run(output1, feed_dict={input1: img})
     get_feature_map(output_feature, 0)
-    
+    s = time.time()
     flag = compressor.fill_buffer(output_feature)
+    print("time for compression: ", time.time() - s)
     if flag is not 0:
         raise "Error"
+    s = time.time()
     decompressed_data = compressor.read_buffer()
-    decompressed_data = np.load("./dev/data/feature_compressed.npy")
+    print("time for decompression: ", time.time() - s)
+    # decompressed_data = np.load("./dev/data/feature_compressed.npy")
     res = sess2.run(output2, feed_dict={input2: decompressed_data})
 
-    bboxes, obj_probs, class_probs = decode_result(model_output=res, output_sizes=(608//32, 608//32), 
-                            num_class=len(class_names), anchors=anchors)
-    
+    bboxes, obj_probs, class_probs = decode_result(model_output=res, output_sizes=(608//32, 608//32),
+                                                   num_class=len(class_names), anchors=anchors)
+
     bboxes, scores, class_max_index = postprocess(
-            bboxes, obj_probs, class_probs, image_shape=img_orig.shape[:2])
+        bboxes, obj_probs, class_probs, image_shape=img_orig.shape[:2])
 
     img_detection = draw_detection(
         img_orig, bboxes, scores, class_max_index, class_names)
     # cv2.imwrite("./data/detection.jpg", img_detection)
     end = time.time()
     print('YOLO_v2 detection has done! spent {} seconds'.format(end - start))
-    
+
     cv2.imshow("detection_results", img_detection)
     cv2.waitKey(0)
     cv2.destroyAllWindows()

dev/test.py
@@ -1,17 +1,11 @@
 import numpy as np
 from detect import *
 import cv2
+import json
 
 if __name__ == "__main__":
-    with open("dev/0_output.bin", mode="rb") as f:
-        filecontent = f.read()
-    print(len(filecontent))
-    data = np.frombuffer(filecontent, np.uint8)
-    f_map = np.reshape(data[0: 76*76], (76, 76))
-    tmp = np.zeros(shape=(76, 76))
-    f_tmp = cv2.normalize(
-                f_map, tmp, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    # f_map = np.expand_dims(f_map, axis=0)
-    # get_feature_map(f_map, is_display=1)
-    cv2.imshow("tmp", f_tmp)
-    cv2.waitKey(0)
+    f = open('./cocoapi/annotations/instances_val2017.json')
+    ann = json.load(f)
+    f = open('./cocoapi/results/results_cut.json')
+    res = json.load(f)
+    print(0)

utils/imgutils.py
@@ -148,10 +148,9 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
+def softmax(x, axis=-1):
     e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
+    return e_x / e_x.sum(axis=axis, keepdims=True)
 
 
 def decode_result(model_output, output_sizes = (19, 19), num_class=80, anchors=None):
@@ -180,7 +179,6 @@ def decode_result(model_output, output_sizes = (19, 19), num_class=80, anchors=N
     return bboxes, obj_probs, class_probs
 
 
-"""
 # depreciated, using decode_result() instead
 def decode(model_output, output_sizes=(19, 19), num_class=80, anchors=None):
     '''
@@ -224,4 +222,3 @@ def decode(model_output, output_sizes=(19, 19), num_class=80, anchors=None):
                        bbox_x+bbox_w/2, bbox_y+bbox_h/2], axis=3)
 
     return bboxes, obj_probs, class_probs
-"""